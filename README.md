1a8f6219ab916785f72f765cdf9ee08c6b857e36
Hassaan Nadeem <hassaan.butt9999@outlook.com>
2020-08-23 15:27:48 +0500

    predict method fixed.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CovidCT
 Detection of COVID-19 by classifying Lungs CT scan
+
+
+Run using flask run --without-threads
--- app.py
+++ app.py
@@ -16,45 +16,47 @@ from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-#Define a Flask app
+# Define a Flask app
 app = Flask(__name__)
 
-MODEL_PATH = 'model.h5'
-model = load_model(MODEL_PATH)
+model = load_model('model.h5')
 model._make_predict_function()
 
 labels = ["COVID negative", "COVID positive"]
 
-def model_predict():
-    #img = image.load_img(img_path, target_size=(150, 150))
 
-    #img = cv2.imread(inImg)
-    #img = cv2.resize(img, (150, 150))
-    img = cv2.imread('uploads/')
+def model_predict(img_path):
+    img = image.load_img(img_path, target_size=(150, 150))
+    img = np.array(img)
     img = np.resize(img, [1, 150, 150, 1])
-    result = model.predict(img)
-    a = labels[int(result[0])]
 
-    return 'The person is ' + a + '.'
+    
+    #print(img)
+    res = model.predict(img)
+    a = labels[int(res[0])]
+    y = '' + a
+    return y
+
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        #get the image
+        # get the image
         f = request.files['file']
 
-        #Save the file to uploads folder
+        # Save the file to uploads folder
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath, 'uploads', secure_filename('1.png'))
+            basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
-        result = model_predict()
+        result = model_predict(file_path)
 
         return result
     return None
